d039d9abd2e87bfd9446911e89c08168039f4973
Hakura77 <36619964+Hakura77@users.noreply.github.com>
2018-02-20 22:02:28 +1300

    Add files via upload
    Beginnings of efficency modifications. Changed .add() method to support being passed an array of lines to be added, and modified renderParty to use only one .out() call.

--- src/View.js
+++ src/View.js
@@ -1,4 +1,11 @@
-class View {
+/* built by Thomas Baines and Kelsey Vavasour
+Forked from code provided by Mike Lance
+Corrected to conform to standard JS 9/11/2017 */
+
+
+/* global DEBUG */
+
+class View { // eslint-disable-line no-unused-vars
   static BLANK () {
     return ''
   }
@@ -11,35 +18,54 @@ class View {
   static NEWLINE () {
     return '<br>'
   }
-  
+
   static DISPBREAK () { // shortened of "Display Break". renders as ', '
     return ',&nbsp;'
   }
-  
+
   static ENDLINE () {
     return '.<br>'
   }
-  
+
   static clr () {
     document.body.style.fontFamily = 'Courier New'
     document.body.innerHTML = ''
   }
+  
   static out (newText) {
     document.body.innerHTML += newText
   }
-  static add (newText) {
-    document.body.innerHTML += '<br>' + newText
-  }
   
+  static add (newText) {
+    // NOTES FOR NEXT TIME: Ask mike about programming standards
+    // NOTES FOR NEXT TIME: Re:usage of '<br>' versus calling this.NEWLINE()
+    let outString = '' // initalize outString
+    // first, check if the passed value is a list
+    if (Array.isArray(newText)) {
+      let outString = ''
+      // the passed value is an array.
+      for (let aLine of newText) {
+        outString += '<br>' + aLine // add method includes break characters on the start of each line.
+      }
+      
+    } else { // assume the item is a string, because its not an array.
+      outString += '<br>' + newText // adding break character to the line
+    }
+    
+    document.body.innerHTML += outString
+  }
+
   static renderParty (aParty) {
     this.add(`${aParty}: ${aParty.totalSeats} Seat(s).${this.NEWLINE()}`)
+    let outString = '' // define storage variable for output values
     for (let aCandidate of aParty.allMyListCandidates) {
       if (aCandidate.isMP()) {
-        this.out(aCandidate)
+        outString += aCandidate // insert aCandidate's to-string into the out storage variable
       }
     }
+    this.out(outString) // push the storage variable into the HTML
   }
-  
+
   static renderElection (anElection) {
     this.add(anElection)
     this.out(this.NEWLINE())
@@ -48,7 +74,7 @@ class View {
     }
     this.out(this.NEWLINE())
   }
-  
+
   static renderElectorateMPComparisons (aCountry, year1, year2) {
     // Renders the comparison between the party of the electorate mp (for each electorate) in the year1 election vs the year2 election.
     let electorates = aCountry.compareElectorateMPParties(year1, year2)
@@ -62,7 +88,7 @@ class View {
     for (let anElectorate of changedElectorates) {
       let outValues = []
       for (let anIndex of anElectorate) {
-        switch(anIndex) {
+        switch (anIndex) {
           case null: // handles if the electorate didn't exist in either election.
             outValues.push('No data')
             break
@@ -73,15 +99,15 @@ class View {
       this.add(`${outValues[0]}: ${this.NEWLINE()}${this.TAB()}${outValues[1]} --> ${outValues[2]}`)
     }
   }
-  
+
   static renderElectoratePartyVoteComparisons (mapVoteData) {
-    // outputs the comparisons between the first year's party vote (by electorate) to the second year's one. 
+    // outputs the comparisons between the first year's party vote (by electorate) to the second year's one.
     // programmer's note: This code is very processor intensive, the view.out calls have a high algorythmic complexity. Possibly investigate storing the values as an out list and pushing them to the view.out in one call.
     this.add(`${this.NEWLINE()}Party Vote Changes, By Electorate:${this.NEWLINE()}`)
-    let nameValues = ['Labor', 'National', 'Other'] // to avoid hard coding in loop. 
+    let nameValues = ['Labor', 'National', 'Other'] // to avoid hard coding in loop.
     for (let anElectorate of mapVoteData.keys()) {
       if (DEBUG) {
-        if (anElectorate == "Taranaki-King Country" || anElectorate == "Taranaki-King") { // manual debug check for anomalous data from election 2014 vs election 2017.
+        if (anElectorate === 'Taranaki-King Country' || anElectorate === 'Taranaki-King') { // manual debug check for anomalous data from election 2014 vs election 2017.
           console.log('Pause for debugging')
         }
       }
@@ -101,7 +127,7 @@ class View {
     }
     this.out(this.NEWLINE())
   }
-  
+
   static renderCountry (aCountry, year1, year2) {
     if (DEBUG) {
       console.log(aCountry.allMyElections)
@@ -113,5 +139,4 @@ class View {
     this.renderElectorateMPComparisons(aCountry, year1, year2)
     this.renderElectoratePartyVoteComparisons(aCountry.compareElectoratePartyVote(year1, year2))
   }
-
 }
